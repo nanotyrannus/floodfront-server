83a5c20f8fd7f99d63dac579c67a4a62ed512407
Ryan Joseph Constantino <ryan.constantino93@gmail.com>
2017-04-11 16:54:13 -0700

    Remove node-side ssl, as it's now the responsibility of nginx. Also, using yarn instead of npm now.

--- Marker.js
+++ Marker.js
@@ -1,4 +1,5 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 class Marker {
     constructor(lat, lon, angle = null) {
         this._lat = lat;
--- main.js
+++ main.js
@@ -1,14 +1,22 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 const Koa = require("koa");
 const config = require("./shared/config");
 const user_routes_1 = require("./routes/user-routes");
 const database_1 = require("./shared/database");
+const https = require("https");
+const enforceSsl = require("koa-sslify");
 const fs = require("fs");
+const send = require("koa-send");
 const body = require("koa-better-body");
 const path = require("path");
 const co = require("co");
 const cors = require("koa-cors");
 const app = new Koa();
+const sslOptions = {
+    "key": fs.readFileSync(config.keyPath),
+    "cert": fs.readFileSync(config.certPath)
+};
 co.wrap(function* () {
     let initialization = fs.readFileSync(`./initialize.pgsql`);
     yield database_1.query(initialization.toString());
@@ -22,9 +30,33 @@ co.wrap(function* () {
     // app.use(function* () {
     //     console.log(this.request.files)
     // })
+    app.use(enforceSsl());
     app.use(cors());
-    app.use(user_routes_1.userRouter.routes());
-    app.listen(config.port);
+    app.use(function* (next) {
+        console.log(`Requesting ${this.path}`);
+        if (this.path === "/") {
+            yield send(this, "index.html", { "root": config.appRoot });
+        }
+        else if (this.path.substring(0, 14) === "/node_modules/") {
+            yield send(this, this.path, { "root": `${config.appRoot}` });
+        }
+        else {
+            try {
+                fs.accessSync(`${config.appRoot}${this.path}`, fs.constants.F_OK);
+                yield send(this, `${this.path}`, { "root": config.appRoot });
+            }
+            catch (e) {
+                console.log(`${this.path} file not found.`);
+                yield next;
+            }
+        }
+        app.use(user_routes_1.userRouter.routes());
+    });
+    // app.listen(config.port)
+    // http.createServer(app.callback()).listen(80)
+    // http.createServer(app.callback()).listen(config.port)
+    https.createServer(sslOptions, app.callback()).listen(443);
+    https.createServer(sslOptions, app.callback()).listen(config.port);
     console.log(`Home: ${config.home}`);
     console.log(`Listening on ${config.port}`);
 })();
