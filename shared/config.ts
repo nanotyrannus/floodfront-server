62b2e961d9f54f9147f20601ca17acc027532245
Ryan Joseph Constantino <ryan.constantino93@gmail.com>
2017-04-12 01:10:03 -0700

    Change port to 8081

--- shared/config.ts
+++ shared/config.ts
@@ -1,4 +1,4 @@
-export const port = 8080 //port for server
+export const port = 8081 //port for server
 export const home = process.env.HOME 
 export const databaseUrl = "postgres://ryan:1234@localhost:5432/floodfront"
 export const databaseName:string  = "floodfront" 
